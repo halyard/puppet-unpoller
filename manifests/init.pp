5f88e41ac97de7a0ff3a12c4603bd89e467b9464
Les Aker <me@lesaker.org>
2023-07-25 14:58:02 -0400

    initial unpoller module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,72 +1,37 @@
 # @summary Install unpoller service
 #
-# @param version sets the unpoller tag to use
+# @param influx_url sets the InfluxDB hostname
+# @param influx_org sets the InfluxDB Organization
+# @param influx_token sets the credential to use for metric submission
+# @param influx_bucket sets the InfluxDB bucket
+# @param loki_url sets the URL for the Loki instance
+# @param loki_user sets the username for Loki auth
+# @param loki_password sets the password for Loki auth
+# @param unifi_url sets the URL for the Unifi instance
+# @param unifi_user sets the username for Unifi auth
+# @param unifi_password sets the password for Unifi auth
 class unpoller (
-  String $version = 'v2.7.13',
+  String $influx_url,
+  String $influx_org,
+  String $influx_token,
+  String $influx_bucket,
+  String $loki_url,
+  String $loki_user,
+  String $loki_password,
+  String $unifi_url,
+  String $unifi_user,
+  String $unifi_password,
 ) {
-  $arch = $facts['os']['architecture'] ? {
-    'x86_64'  => 'amd64',
-    'arm64'   => 'armv7',
-    'aarch64' => 'armv7',
-    'arm'     => 'armv7',
-    default   => 'error',
-  }
-
-  $binfile = '/usr/local/bin/unpoller'
-  $fileversion = regsubst($version, '^v.*$', '\1')
-  $filename = "unpoller_${fileversion}_${downcase($facts['kernel'])}_${arch}.tar.gz"
-  $url = "https://github.com/akerl/slack-mastodon/releases/download/${version}/${filename}"
-
-  group { 'unpoller':
-    ensure => present,
-    system => true,
-  }
-
-  user { 'unpoller':
-    ensure => present,
-    system => true,
-    gid    => 'unpoller',
-    shell  => '/usr/bin/nologin',
-    home   => '/var/lib/unpoller',
-  }
-
-  exec { 'download unpoller':
-    command => "/usr/bin/curl -sLo '${binfile}' '${url}' && chmod a+x '${binfile}'",
-    unless  => "/usr/bin/test -f ${binfile} && ${binfile} version | grep '${version}'",
-  }
-
-  file { [
-      '/var/lib/unpoller',
-      '/var/lib/unpoller/.config',
-      '/var/lib/unpoller/.config/slack-mastodon',
-    ]:
-      ensure => directory,
-      owner  => 'unpoller',
-      group  => 'unpoller',
-      mode   => '0750',
-  }
-
-  file { '/var/lib/unpoller/.config/slack-mastodon/config.yml':
+  file { '/etc/unpoller.conf':
     ensure  => file,
-    mode    => '0640',
-    owner   => 'unpoller',
-    group   => 'unpoller',
-    content => template('unpoller/config.yml.erb'),
+    content => template('unpoller/unpoller.conf.erb'),
   }
 
-  file { '/etc/systemd/system/slack-mastodon.service':
-    ensure => file,
-    source => 'puppet:///modules/unpoller/slack-mastodon.service',
-  }
-
-  file { '/etc/systemd/system/slack-mastodon.timer':
-    ensure  => file,
-    content => template('unpoller/slack-mastodon.timer.erb'),
-  }
-
-  ~> service { 'slack-mastodon.timer':
-    ensure  => running,
-    enable  => true,
-    require => File['/var/lib/unpoller/.config/slack-mastodon/config.yml'],
+  ~> docker::container { 'unpoller':
+    image => 'ghcr.io/unpoller/unpoller:latest',
+    args  => [
+      '-v /etc/unpoller.conf:/etc/unpoller/up.conf',
+    ],
+    cmd   => '',
   }
 }
